763e404872b5f63dc295444e00a1bdd0f24d9f58
Jayden <jayden.hong@goat.com>
2022-01-12 18:38:50 +0800

    feature

--- logger_test.go
+++ logger_test.go
@@ -464,21 +464,3 @@ func TestLogger_FatalWithHighThanINFOLevel(t *testing.T) {
 	h.AssertExpectations(t)
 	e.AssertExpectations(t)
 }
-
-/*
-
-
-
-func TestLogger_Caller(t *testing.T) {
-	l := newLogger()
-	l.EnableCaller(INFO)
-
-	e := l.Info()
-	e.Log()
-
-	assert.True(t, e.OK)
-	assert.True(t, e.PC > 0)
-
-	l.DisableCaller(INFO)
-}
-*/
